363143d53330bb4b0c4a2750da0ea03075a66bae
dma <dma@globaz.ch>
2018-04-20 01:06:07 +0200

    Ajout d'une interface node

--- src/main/java/ch/hesso/xmleditor/map/Node.java
+++ src/main/java/ch/hesso/xmleditor/map/Node.java
@@ -3,11 +3,30 @@ package ch.hesso.xmleditor.map;
 import java.util.List;
 
 public interface Node {
+    /**
+     * Permet d'avoir l'identifiant du noeud.
+     * @return L'idendifiant du noeud.
+     */
     String getId();
 
+    /**
+     * Retourne le text du noeud courant.
+     *
+     * @return le text du noeud.
+     */
     String getName();
 
+    /**
+     * Retourne le nom du tag du noeud.
+     *
+     * @return le nom tu tag.
+     */
     String getText();
 
+    /**
+     * Retourne la liste des enfants du noeud.
+     *
+     * @return La liste de ces enfants.
+     */
     List<NodeImpl> getChildren();
 }
