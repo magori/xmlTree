52167790dc3e1c472bb87c19570319394d8eb133
Noémien Kocher <nkcr.je@gmail.com>
2018-05-03 16:36:13 +0200

    Properties for persistence

--- src/main/java/ch/hesso/xmleditor/display/XmlEditorManagerModuleGuice.java
+++ src/main/java/ch/hesso/xmleditor/display/XmlEditorManagerModuleGuice.java
@@ -6,14 +6,33 @@ import ch.hesso.xmleditor.map.Mapper;
 import ch.hesso.xmleditor.map.MapperImpl;
 import ch.hesso.xmleditor.persistence.Persister;
 import ch.hesso.xmleditor.persistence.PersisterDbImpl;
-import ch.hesso.xmleditor.persistence.PersisterFileImpl;
 import com.google.inject.AbstractModule;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 class XmlEditorManagerModuleGuice extends AbstractModule {
     @Override
     protected void configure() {
         bind(Mapper.class).to(MapperImpl.class);
         bind(Manipulater.class).to(ManipulaterJsonImpl.class);
+
+        // create and load default properties
+        Properties defaultProps = new Properties();
+        try {
+            FileInputStream in = new FileInputStream("./config/config.properties");
+            defaultProps.load(in);
+            // create application properties with default
+            Properties applicationProps = new Properties(defaultProps);
+            applicationProps.load(in);
+            System.out.println(applicationProps.getProperty("persister"));
+            in.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+
         bind(Persister.class).to(PersisterDbImpl.class);
     }
 }
