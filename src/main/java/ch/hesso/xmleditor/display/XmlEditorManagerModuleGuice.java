8a1824fd083fae304a503d3481fd1a8b975e160f
dma <dma@globaz.ch>
2018-05-03 18:28:19 +0200

    db

--- src/main/java/ch/hesso/xmleditor/display/XmlEditorManagerModuleGuice.java
+++ src/main/java/ch/hesso/xmleditor/display/XmlEditorManagerModuleGuice.java
@@ -11,9 +11,6 @@ import com.google.inject.multibindings.MapBinder;
 import org.jooq.SQLDialect;
 
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
